54d914ccaf9c3c5c9eda763ad72260b915f60dfa
Cristi R <christin.rusnac@bycape.io>
2023-06-24 13:24:28 +0200

    Add: navbar & routing

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.scss';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+root.render(
+    <BrowserRouter>
+        <App tab="home" />
+    </BrowserRouter>
+);
+
+// const rootElement = document.getElementById("root");
 
 // ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
+//     <React.StrictMode>
+//         <BrowserRouter>
+//             <App />
+//         </BrowserRouter>
+//     </React.StrictMode>,
+//     rootElement
 // );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from 'react';
+import { ReactComponent as Logo } from '../../assets/RENEGVDE.svg'
+import { Link, Outlet } from 'react-router-dom';
+
+import './navigation.styles.scss'
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className='navigation'>
+                <Link className='logo-container' to="/">
+                    <div>
+                        <Logo className='logo' />
+                    </div>
+                </Link>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to="/shop">SHOP</Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default Navigation
