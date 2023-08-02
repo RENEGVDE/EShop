0ac4938579caf6576cad5a19a8f75bc4e9ed52c5
RENEGVDE <dcgws21rki@yahoo.com>
2023-08-02 15:12:34 +0200

    Refactor: typescript

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -26,8 +26,9 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  // apiKey: `${process.env.REACT_APP_API_KEY}`,
   // apiKey: `${secrets.REACT_APP_API_KEY}`,
+  apiKey: "AIzaSyBqgJbGXjBpCjZPQIidD9NMmXKLqvlU91Y",
   authDomain: "renegvde-eshop.firebaseapp.com",
   projectId: "renegvde-eshop",
   storageBucket: "renegvde-eshop.appspot.com",
