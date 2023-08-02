22efc6522f71839380a6cf76895f161610d256c3
RENEGVDE <dcgws21rki@yahoo.com>
2023-08-02 18:11:59 +0200

    Refactor: stripe public key

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,6 +1,7 @@
 import { loadStripe } from "@stripe/stripe-js";
 
 export const stripePromise = loadStripe(
-  `${process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}`
-//   `${secrets.REACT_APP_STRIPE_PUBLISHABLE_KEY}`
+  "pk_test_51L3lnpLMdfQvARVFzI1Wc5ifOnCLstLIT0dtSGbi6gQpldYXwFsALex9JSPc8DD2BKCWPF2YCIaMDwSHekZEPX2000BjyabhnM"
+  // `${process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}`
+  //   `${secrets.REACT_APP_STRIPE_PUBLISHABLE_KEY}`
 );
